f5cdcb1473213754751b3bfe94161e08be56749b
Andrey "Limych" Khrolenok <andrey@khrolenok.ru>
2019-02-16 23:52:34 +0300

    Added detailed description

--- antizapret.pl
+++ antizapret.pl
@@ -1,5 +1,8 @@
 #!/usr/local/bin/perl
 
+use strict;
+use warnings FATAL => 'all';
+
 my @ips = `fetch -o - https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv | sed 1d | cut -d ';' -f 1 | tr "\\|" "\\n" | sed 's/^[ \\t]*//;s/[ \\t]*\$//' | sort | uniq`;
 
 my $buf = '';
@@ -26,8 +29,8 @@ foreach $ip (@ips) {
 }
 
 
+
 sub starts_with
 {
     return substr($_[0], 0, length($_[1])) eq $_[1];
 }
-
