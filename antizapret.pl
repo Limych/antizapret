4d62b5184fc6991f418a34d72cd88b61e963ac11
Andrey Khrolenok <andrey@khrolenok.ru>
2020-05-11 22:51:35 +0300

    Fix small bug (#2)

--- antizapret.pl
+++ antizapret.pl
@@ -3,14 +3,17 @@
 use strict;
 use warnings FATAL => 'all';
 
+$| = 1;
+
 my @ips = `fetch -o - https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv | sed 1d | LC_CTYPE=C cut -d ';' -f 1 | tr "\\|" "\\n" | sed 's/^[ \\t]*//;s/[ \\t]*\$//' | sort | uniq`;
 
 my $buf = '';
 my $cnt = 0;
-my $mask = '';
+my $mask = undef;
 
 foreach my $ip (@ips) {
-    if ($mask eq '') {
+    next if $ip eq "\n";
+    if (!defined $mask) {
         ($mask) = ($ip =~ /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.)/);
         $buf = $ip;
         $cnt = 1;
