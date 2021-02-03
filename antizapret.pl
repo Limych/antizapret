a8fc07377a2e1e4eb3fcda3b35c4f74d10bd572a
Andrey Khrolenok <andrey@khrolenok.ru>
2021-02-03 19:07:07 +0300

    Make main script universal

--- antizapret.pl
+++ antizapret.pl
@@ -5,7 +5,19 @@ use warnings FATAL => 'all';
 
 $| = 1;
 
-my @ips = `fetch -o - https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv | sed 1d | LC_CTYPE=C cut -d ';' -f 1 | tr "\\|" "\\n" | sed 's/^[ \\t]*//;s/[ \\t]*\$//' | sort | uniq`;
+my $fetcher = `which fetch curl wget`;
+$fetcher =~ s/^(\S+)\s.*$/$1/s;
+if ($fetcher =~ /fetch/) {
+    $fetcher .= ' -o -'
+} elsif ($fetcher =~ /curl/) {
+    # pass
+} elsif ($fetcher =~ /wget/) {
+    $fetcher .= ' -O -'
+} else {
+    die "ERROR: Can't find a program to download data.";
+}
+
+my @ips = `$fetcher "https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv" | sed 1d | LC_CTYPE=C cut -d ';' -f 1 | tr "\\|" "\\n" | sed 's/^[ \\t]*//;s/[ \\t]*\$//' | sort | uniq`;
 
 my $buf = '';
 my $cnt = 0;
