93ffff466fcb9c715c556432e9c44697ac4cf035
Andrey Khrolenok <andrey@khrolenok.ru>
2021-02-04 18:55:11 +0300

    Add IPv6 optimization

--- antizapret.pl
+++ antizapret.pl
@@ -2,41 +2,62 @@
 
 use strict;
 use warnings FATAL => 'all';
+use Socket qw(inet_pton AF_INET6);
 
 $| = 1;
 
-my $fetcher = `which fetch curl wget`;
-$fetcher =~ s/^(\S+)\s.*$/$1/s;
-if ($fetcher =~ /fetch/) {
-    $fetcher .= ' -o -'
-} elsif ($fetcher =~ /curl/) {
-    # pass
-} elsif ($fetcher =~ /wget/) {
-    $fetcher .= ' -O -'
-} else {
-    die "ERROR: Can't find a program to download data.";
+my $fetcher;
+if (@ARGV) {
+    $fetcher = 'cat "' . $ARGV[0] . '"';
+}
+else {
+    $fetcher = `which fetch curl wget`;
+    $fetcher =~ s/^(\S+)\s.*$/$1/s;
+    if ($fetcher =~ /fetch/) {
+        $fetcher .= ' -o -'
+    }
+    elsif ($fetcher =~ /curl/) {
+        # pass
+    }
+    elsif ($fetcher =~ /wget/) {
+        $fetcher .= ' -O -'
+    }
+    else {
+        die "ERROR: Can't find a program to download data.";
+    }
+    $fetcher .= ' "https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv"';
 }
 
-my @ips = `$fetcher "https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv" | sed 1d | LC_CTYPE=C cut -d ';' -f 1 | tr "\\|" "\\n" | sed 's/^[ \\t]*//;s/[ \\t]*\$//' | sort | uniq`;
+my @ips = `$fetcher | sed 1d | LC_CTYPE=C cut -d ';' -f 1 | tr "\\|" "\\n" | sed 's/^[ \\t]*//;s/[ \\t]*\$//' | sort | uniq`;
 
 my $buf = '';
 my $cnt = 0;
 my $mask = undef;
+my ($ip_norm, $isv6);
 
-foreach my $ip (@ips) {
-    next if $ip eq "\n" or $ip !~ /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}$/;
-    if (defined $mask && substr($ip, 0, length($mask)) eq $mask) {
+foreach my $ip (map {lc} @ips) {
+    next if ($ip eq "\n");
+    $ip_norm = $ip;
+    chomp($ip_norm);
+    $isv6 = ($ip_norm =~ /:/);
+    $ip_norm = join(":", unpack("H4H4H4H4H4H4H4H4", inet_pton(AF_INET6, $ip_norm))) if ($isv6);
+    if (defined $mask && substr($ip_norm, 0, length($mask)) eq $mask) {
         $buf .= $ip;
         $cnt++;
     }
     else {
         if ($cnt >= 10) {
-            print $mask . "0/24\n";
+            print($mask !~ /:/ ? "${mask}0/24\n" : "${mask}00/108\n");
         }
         else {
             print $buf;
         }
-        ($mask) = ($ip =~ /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.)/);
+        if (!$isv6) {
+            ($mask) = ($ip_norm =~ /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.)/);
+        }
+        else {
+            ($mask) = ($ip_norm =~ /^((?:[0-9a-f]{4}\:){7}[0-9a-f]{2})/);
+        }
         $buf = $ip;
         $cnt = 1;
     }
